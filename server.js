41f5695b74936f5b46ae08d803a284d918bf6567
Dan <dan@Macs-MacBook-Pro.local>
2018-02-04 23:41:20 +0000

    serv static files server.js

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Add Api routes
 require('./server/routes/api')(app);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
