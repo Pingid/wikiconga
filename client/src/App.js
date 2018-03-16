cb9a56c632d07b7fce06b4fde2218360744bf489
Dan <dan@Macs-MacBook-Pro.local>
2018-03-16 16:24:50 +0000

    click on poem lines

--- client/src/App.js
+++ client/src/App.js
@@ -38,17 +38,27 @@ const Meta = styled.div`
   line-height: 13px
 `
 
+const PoemLine = styled.span`
+  cursor: pointer;
+  margin: 2px 0px;;
+  color: black;
+  transition: .1s color;
+  &:hover {
+    color: red;
+  }
+`
+
 const aboutMarkdown = `Inspired by the [Oulipian](http://oulipo.net/) poem structure ['Littérature définitionnelle'](http://oulipo.net/fr/contraintes/litterature-definitionnelle), developed by [Raymond Queneau](https://en.wikipedia.org/wiki/Raymond_Queneau), [Marcel Breuer](https://en.wikipedia.org/wiki/Marcel_Breuer) and [Georges Perec](https://en.wikipedia.org/wiki/Georges_Perec), which recursively generates poems by substituting words for there dictionary definitions. This tool generates poems by taking the words up until the first hyperlink on a Wikipedia page and then following that hyperlink and concatenating the initial text from the next link, it repeats this process recursively until a page is repeated.`
 
 class App extends Component {
   state = { 
-    search: '',
+    search: 'https://en.wikipedia.org/wiki/Oulipo',
     poem: [],
     network: { fetching: false, err: '' },
     currentWiki: ''
   };
   render() {
-    const { search, poem, network } = this.state;
+    const { search, poem, network, currentWiki } = this.state;
     return (
       <Wrapper className="">
         <h4 className="m0 mt3">Wikiconga</h4>
@@ -65,7 +75,7 @@ class App extends Component {
         <Form className="flex items-center" style={{ maxWidth: '30rem' }} onSubmit={this._handleGenerate.bind(this)}>
           <Input
             type="text"
-            value={search}
+            value={currentWiki.length > 1 ? currentWiki : search}
             placeholder="https://en.wikipedia.org/wiki/Wiki"
             className="p2 caps"
             onChange={(e) => this.setState({ search: e.target.value })} />
@@ -76,28 +86,41 @@ class App extends Component {
           </GenerateButton>
         </Form>
         <p className="m0 pl2 flex items-center" style={{ color: 'red', fontSize: '.5rem', height: '2rem' }}>{network.err}</p>
-        <p className="mt0" style={{ minHeight: '27vh' }}>
+        <p className="mt0" style={{ 
+          minHeight: '27vh', 
+          opacity: network.fetching ? .5 : 1,
+          pointerEvents: network.fetching ? 'none' : 'auto' }}>
           {
-            poem.map((x, i) => (
-              <span
+            poem.map((x, i) => ([
+              <PoemLine
                 key={i}
+                onClick={this._handlePoemClick.bind(this)}
                 onMouseEnter={() => this.setState({ currentWiki: x.href })} 
                 onMouseLeave={() => this.setState({ currentWiki: '' })}>
-                <a href={x.href}>{x.text}</a><br />
-              </span>
-            ))
+                {x.text}
+              </PoemLine>,
+              <br key={'br-'+i}/>
+            ]))
           }
         </p>
       </Wrapper>
     );
   }
+  _handlePoemClick(e) {
+    const { currentWiki, search } = this.state;
+    this.state.search = currentWiki;
+    this._handleGenerate.call(this, e);
+  }
   _handleGenerate(e) {
-    const { search } = this.state;
-    const isWikiPage = new RegExp('wikipedia.org\/wiki', 'gi')
     if (e) { e.preventDefault() }
+
+    const { search, currentWiki } = this.state;
+    const isWikiPage = new RegExp('wikipedia.org\/wiki', 'gi')
+
     if (isWikiPage.test(search)) {
       return this._getPoem(search)
     }
+
     return this.setState({ 
       network: { 
         fetching: false, 
